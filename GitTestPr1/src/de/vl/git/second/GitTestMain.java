818eec314a4cb859d590728cd55d9464b0799952
Vladimir Limar <livl@gmx.de>
2013-10-09 23:37:37 +0200

    Change Main class

--- GitTestPr1/src/de/vl/git/second/GitTestMain.java
+++ GitTestPr1/src/de/vl/git/second/GitTestMain.java
@@ -4,7 +4,15 @@ public class GitTestMain {
 
 	public static void main(String[] args) {
 		System.out.println("Git is cool");
+		GitTestMain thisClass = new GitTestMain();
+		int i=1;
+		thisClass.testParameterMethod(i++, i);
+		thisClass.testParameterMethod(i, i++);
 
+	}
+	
+	public void testParameterMethod(int p1, int p2){
+		System.out.println("p1 = "+p1+"; p2 = "+p2);
 	}
 
 }
